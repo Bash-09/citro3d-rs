286e4f369e3ff20e0fb749dd18a8ec7ef6fbeaf9
Bash <Discord @bash09>
2024-09-28 15:33:49 +1000

    Texture example

---
+++ citro3d/examples/textured.rs
@@ -0,0 +1,245 @@
+//! This example demonstrates the most basic usage of `citro3d`: rendering a simple
+//! RGB triangle (sometimes called a "Hello triangle") to the 3DS screen.
+
+#![feature(allocator_api)]
+
+use citro3d::macros::include_shader;
+use citro3d::math::{AspectRatio, ClipPlanes, Matrix4, Projection, StereoDisplacement};
+use citro3d::render::ClearFlags;
+use citro3d::{attrib, buffer, shader, texture};
+use citro3d::{texenv, RenderParameters, RenderPass};
+use ctru::prelude::*;
+use ctru::services::gfx::{RawFrameBuffer, Screen, TopScreen3D};
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Vec3 {
+    const fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct Vec2 {
+    x: f32,
+    y: f32,
+}
+
+impl Vec2 {
+    const fn new(x: f32, y: f32) -> Self {
+        Self { x, y }
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct Vertex {
+    pos: Vec3,
+    tex: Vec2,
+}
+
+static VERTICES: &[Vertex] = &[
+    Vertex {
+        pos: Vec3::new(0.5, 0.5, -3.0),
+        tex: Vec2::new(1.0, 1.0),
+    },
+    Vertex {
+        pos: Vec3::new(-0.5, 0.5, -3.0),
+        tex: Vec2::new(0.0, 1.0),
+    },
+    Vertex {
+        pos: Vec3::new(-0.5, -0.5, -3.0),
+        tex: Vec2::new(0.0, 0.0),
+    },
+    Vertex {
+        pos: Vec3::new(0.5, 0.5, -3.0),
+        tex: Vec2::new(1.0, 1.0),
+    },
+    Vertex {
+        pos: Vec3::new(-0.5, -0.5, -3.0),
+        tex: Vec2::new(0.0, 0.0),
+    },
+    Vertex {
+        pos: Vec3::new(0.5, -0.5, -3.0),
+        tex: Vec2::new(1.0, 0.0),
+    },
+];
+
+static SHADER_BYTES: &[u8] = include_shader!("assets/vshader_textured.pica");
+static TEXTURE_BYTES: &[u8] = include_bytes!("assets/logo.bin");
+const CLEAR_COLOR: u32 = 0x68_B0_D8_FF;
+
+fn main() {
+    let mut soc = Soc::new().expect("failed to get SOC");
+    drop(soc.redirect_to_3dslink(true, true));
+
+    let gfx = Gfx::new().expect("Couldn't obtain GFX controller");
+    let mut hid = Hid::new().expect("Couldn't obtain HID controller");
+    let apt = Apt::new().expect("Couldn't obtain APT controller");
+
+    let mut instance = citro3d::Instance::new().expect("failed to initialize Citro3D");
+
+    let top_screen = TopScreen3D::from(&gfx.top_screen);
+
+    let (mut top_left, mut top_right) = top_screen.split_mut();
+
+    let RawFrameBuffer { width, height, .. } = top_left.raw_framebuffer();
+    let mut top_left_target = instance
+        .render_target(width, height, top_left, None)
+        .expect("failed to create render target");
+
+    let RawFrameBuffer { width, height, .. } = top_right.raw_framebuffer();
+    let mut top_right_target = instance
+        .render_target(width, height, top_right, None)
+        .expect("failed to create render target");
+
+    let mut bottom_screen = gfx.bottom_screen.borrow_mut();
+    let RawFrameBuffer { width, height, .. } = bottom_screen.raw_framebuffer();
+
+    let mut bottom_target = instance
+        .render_target(width, height, bottom_screen, None)
+        .expect("failed to create bottom screen render target");
+
+    let program = {
+        let shader = shader::Library::from_bytes(SHADER_BYTES).unwrap();
+        shader::Program::new(shader, 0).unwrap()
+    };
+
+    let mut vbo_data = Vec::with_capacity_in(VERTICES.len(), ctru::linear::LinearAllocator);
+    vbo_data.extend_from_slice(VERTICES);
+
+    let mut buf_info = buffer::Info::new();
+    let (attr_info, vbo_data) = prepare_vbos(&mut buf_info, &vbo_data);
+
+    let tex = create_texture();
+
+    // Configure the first fragment shading substage to just pass through the vertex color
+    // See https://www.opengl.org/sdk/docs/man2/xhtml/glTexEnv.xml for more insight
+    let stage0 = texenv::TexEnv {
+        func: texenv::Func {
+            mode: texenv::Mode::BOTH,
+            combine: texenv::CombineFunc::Replace,
+        },
+        sources: texenv::Sources {
+            mode: texenv::Mode::BOTH,
+            source0: texenv::Source::Texture0(&tex),
+            source1: None,
+            source2: None,
+        },
+        ops: Default::default(),
+    };
+
+    let projection_uniform_idx = program.get_uniform("projection").unwrap();
+
+    while apt.main_loop() {
+        hid.scan_input();
+
+        if hid.keys_down().contains(KeyPad::START) {
+            break;
+        }
+
+        let projections = calculate_projections();
+        let targets = [
+            (&mut top_left_target, projections.left_eye),
+            (&mut top_right_target, projections.right_eye),
+            (&mut bottom_target, projections.center),
+        ];
+
+        instance.render_frame_with(|frame| {
+            for (target, proj) in targets {
+                target.clear(ClearFlags::ALL, CLEAR_COLOR, 0);
+
+                let pass = RenderPass {
+                    program: &program,
+                    target,
+                    vbo_data,
+                    attribute_info: &attr_info,
+                    texenv_stages: &[stage0],
+                    params: RenderParameters {
+                        primitive: buffer::Primitive::Triangles,
+                        vertex_uniforms: &[(projection_uniform_idx, proj.into())],
+                        ..Default::default()
+                    },
+                };
+
+                frame.draw(pass).unwrap();
+            }
+        });
+    }
+}
+
+fn prepare_vbos<'a>(
+    buf_info: &'a mut buffer::Info,
+    vbo_data: &'a [Vertex],
+) -> (attrib::Info, buffer::Slice<'a>) {
+    // Configure attributes for use with the vertex shader
+    let mut attr_info = attrib::Info::new();
+
+    let reg0 = attrib::Register::new(0).unwrap();
+    let reg1 = attrib::Register::new(1).unwrap();
+
+    attr_info
+        .add_loader(reg0, attrib::Format::Float, 3)
+        .unwrap();
+
+    attr_info
+        .add_loader(reg1, attrib::Format::Float, 2)
+        .unwrap();
+
+    let buf_idx = buf_info.add(vbo_data, &attr_info).unwrap();
+
+    (attr_info, buf_idx)
+}
+
+// Make a little checkered grid
+fn create_texture() -> texture::Texture {
+    let params = texture::TextureParameters::new_2d(64, 64, texture::Format::RGBA8);
+    let mut tex = texture::Texture::new(params).unwrap();
+    tex.load_image(TEXTURE_BYTES, texture::Face::default())
+        .unwrap();
+    tex.set_filter(texture::Filter::Linear, texture::Filter::Nearest);
+    tex
+}
+
+struct Projections {
+    left_eye: Matrix4,
+    right_eye: Matrix4,
+    center: Matrix4,
+}
+
+fn calculate_projections() -> Projections {
+    // TODO: it would be cool to allow playing around with these parameters on
+    // the fly with D-pad, etc.
+    let slider_val = ctru::os::current_3d_slider_state();
+    let interocular_distance = slider_val / 2.0;
+
+    let vertical_fov = 40.0_f32.to_radians();
+    let screen_depth = 2.0;
+
+    let clip_planes = ClipPlanes {
+        near: 0.01,
+        far: 100.0,
+    };
+
+    let (left, right) = StereoDisplacement::new(interocular_distance, screen_depth);
+
+    let (left_eye, right_eye) =
+        Projection::perspective(vertical_fov, AspectRatio::TopScreen, clip_planes)
+            .stereo_matrices(left, right);
+
+    let center =
+        Projection::perspective(vertical_fov, AspectRatio::BottomScreen, clip_planes).into();
+
+    Projections {
+        left_eye,
+        right_eye,
+        center,
+    }
+}
